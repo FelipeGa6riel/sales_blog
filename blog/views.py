0ff44d7ab9dddb8cf6010453ca0355853d2d2d10
Felipe Gabriel <felipegabriel.play@gmail.com>
2025-09-08 21:28:50 -0300

    feat: add filter query in url for post tags

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
+
 from blog.models import Post
+
 
 # Create your views here.
 def index_posts(request):
+    if request.GET.get("tag"):
+        url_qr = request.GET.get("tag")
+        posts = Post.objects.filter(tags__name=url_qr)
+
+        return render(request, "index.html", {"posts": posts})
+
     posts = Post.objects.all()
 
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, "index.html", {"posts": posts})
 
 
 def post_detail(request, pk, title):
     post = Post.objects.get(pk=pk)
 
-    return render(request, 'post_detail.html', {'post': post})
+    return render(request, "post_detail.html", {"post": post})
+
+
+def post_by_tag(request):
+    url_qr = request.GET.get("tag")
+    posts = Post.objects.get_queryset().filter(url_qr)
+    return render(request, "index.html", {"posts": posts})
